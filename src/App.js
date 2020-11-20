394238a896be057a17411861776d38d23056114b
Clayton C <clayton@hansensoftware.com>
2020-11-19 16:10:27 -0800

    n11

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
+import Scroll from './Scroll';
 
 const state = {
     robots: robots,
@@ -33,7 +34,9 @@ class App extends Component{
             <div className='tc'>
                 <h1 className='f2' >RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
                     <CardList robots={filteredRobots}/>
+                    </Scroll>
             </div>
         );
     }   
